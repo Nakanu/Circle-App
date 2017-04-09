32d734208f69a77179b9226575507a5951aeea4c
wo-amlangwang <adamlangwang@gmail.com>
2017-04-09 17:13:54 -0400

    add login script

---
+++ js/login.js
@@ -0,0 +1,29 @@
+function signUp() {
+
+	var userName = document.getElementById('userName').value;
+	var passWord = document.getElementById('passWord').value;
+	var repassowrd = document.getElementById('rePassword').value;
+	if(passWord === repassowrd){
+		var socket = io.connect();
+		var data = {
+			userName : userName,
+			password : password
+		}
+		socket.emit('register',data);
+		socket.on('register',function (data) {
+			console.log(data);
+			if(data.result == 'success'){
+				
+				document.cookie = "token=" + data.data.token;
+				//TODO
+				document.getElementById('sign-up').style.display='none'
+			}else{
+				//TODO
+			}
+		})
+		//socket.disconnect();
+	}else{
+		//TODO
+	}
+}
+
