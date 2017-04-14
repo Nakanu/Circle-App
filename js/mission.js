8b4fe6c26544650ff3c0f6eaf673e1d5044ec315
JunmingGuo <sjunmg@gmail.com>
2017-04-13 20:40:31 -0400

    add publish_new_task_page

---
+++ js/mission.js
@@ -0,0 +1,16 @@
+// Get the modal
+var modal = document.getElementById("add-request");
+
+// When user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if(event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+
+// Get Sign-up button and when user click on it, display sign-up form
+document.getElementById("new-request-btn").onclick = function() { showSignUpForm() };
+
+function showSignUpForm() {
+	document.getElementById("add-request").style.display = "block";
+}
